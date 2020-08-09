781b699155d272d769e03523924a297ac4de097a
Felipe Costa <githubfelipecosta@outlook.com>
2020-08-09 18:27:01 +0400

    node-app

---
+++ node-app/index.js
@@ -0,0 +1,9 @@
+require ('trend_app_protect');
+require('dotenv/config')
+
+const app = require('./config/custom-express');
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is on at http://${HOST}:${PORT}`));
